bd43f51bc292c7d3bc7bbf49545c2006aaccfbe1
Michael Toback <michael.toback@gmail.com>
2020-04-19 08:40:28 -0700

    remove ssl for now, clean up format on welcome page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-
+  showWhoIAm = false;
+  showWhatElse = false;
   constructor() { }
 
   ngOnInit(): void {
